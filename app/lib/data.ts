d0cd69c0c13d41f43bf6889d6cd45151fac54327
Mark W <markwuu@gmail.com>
2025-11-17 17:39:40 -0800

    simulated slow requests

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -18,7 +18,10 @@ export async function fetchRevenue() {
 		// Don't do this in production :)
 
 		// console.log('Fetching revenue data...');
-		// await new Promise((resolve) => setTimeout(resolve, 3000));
+		// 🚀 IMPORTANT CONCEPT ON DEMONSTRATING SUSPENSE 🚀
+		// simulating slow request to demonstrate Suspense
+		// component only renders once data is returned
+		await new Promise((resolve) => setTimeout(resolve, 3000));
 
 		const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
@@ -33,6 +36,9 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
 	try {
+		// 🚀 IMPORTANT CONCEPT ON DEMONSTRATING SUSPENSE 🚀
+		await new Promise((resolve) => setTimeout(resolve, 5000));
+
 		const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -53,6 +59,9 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
 	try {
+		// 🚀 IMPORTANT CONCEPT ON DEMONSTRATING SUSPENSE 🚀
+		await new Promise((resolve) => setTimeout(resolve, 2000));
+
 		// You can probably combine these into a single SQL query
 		// However, we are intentionally splitting them to demonstrate
 		// how to initialize multiple queries in parallel with JS.
