ccbac6e223cbc69f5268720e05187acfb988d95a
Mark W <markwuu@gmail.com>
2025-11-19 15:29:00 -0800

    created 404 page for invoices

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 // 🚀 IMPORTANT CONCEPT 🚀 //
 // Page also accepts props called params, so you have access to dynamic route params
@@ -11,6 +12,10 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
 		fetchInvoiceById(id),
 		fetchCustomers(),
 	]);
+
+	if (!invoice) {
+		notFound();
+	}
 
 	return (
 		<main>
