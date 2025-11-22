b24f40f2ba04fe48a2a9fbae08c6960c2380e18d
Mark W <markwuu@gmail.com>
2025-11-22 15:37:40 -0800

    created login page

---
+++ app/login/page.tsx
@@ -0,0 +1,25 @@
+// 🚀 CHAPTER 14: AUTHENTICATION CONCEPT 🚀 //
+// Create a login page for users to signin/authenticate
+// LoginForm wrapped in Suspense because
+// it accesses information from incoming request (URL search params)
+
+import AcmeLogo from '@/app/ui/acme-logo';
+import LoginForm from '@/app/ui/login-form';
+import { Suspense } from 'react';
+
+export default function LoginPage() {
+	return (
+		<main className="flex items-center justify-center md:h-screen">
+			<div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+				<div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+					<div className="w-32 text-white md:w-36">
+						<AcmeLogo />
+					</div>
+				</div>
+				<Suspense>
+					<LoginForm />
+				</Suspense>
+			</div>
+		</main>
+	);
+}
